37bc28815f0d5a2a0364feedab4e29fc25c82563
Davis Robertson <davis@daviskr.com>
2015-02-22 00:50:23 -0600

    Adding configuration for the host and port
    Adding configuration in config.py to specify the host and port to run
    the server on.

--- config.py
+++ config.py
@@ -2,3 +2,6 @@
 db_name = "sqlite:///urls.db"
 
 subdomains = ["f", "sk", "d", "g", "j", "h", "sp"]
+
+host = "127.0.0.1"
+port = 9999
--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 from SDSURLS import app
+import config
 
 if __name__ == "__main__":
     from wsgiref import simple_server
-    httpd = simple_server.make_server("127.0.0.1", 8000, app)
+    httpd = simple_server.make_server(config.host, config.port, app)
     httpd.serve_forever()
